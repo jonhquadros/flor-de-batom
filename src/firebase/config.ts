01a04b0641dd53bf4fa278d32e6fa8379938cb89
Jonh Quadros <jonhquadros@gmail.com>
2026-04-14 17:15:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2246415637-63ad0",
+  "appId": "1:884493331731:web:5d7474818703410ff67ee6",
+  "apiKey": "AIzaSyBMyGharLagDJuIAIyneBGAJIFF6tbrO4Y",
+  "authDomain": "studio-2246415637-63ad0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "884493331731"
+};
