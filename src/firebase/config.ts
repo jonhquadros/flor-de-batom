5daf7c72f209ebb0b5b34a8624a4d6e8182ff886
Jonh Quadros <jonhquadros@gmail.com>
2026-05-26 20:06:41 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:884493331731:web:5d7474818703410ff67ee6",
   "apiKey": "AIzaSyBMyGharLagDJuIAIyneBGAJIFF6tbrO4Y",
   "authDomain": "studio-2246415637-63ad0.firebaseapp.com",
+  "storageBucket": "studio-2246415637-63ad0.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "884493331731"
 };
